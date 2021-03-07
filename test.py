c053439476733d5be8878b014ce02215c5831a02
Walter Rowe <walter.rowe@gmail.com>
2021-03-07 14:19:34 -0500

    testing dictionaries in python

--- test.py
+++ test.py
@@ -2,45 +2,19 @@
 ## https://linuxhint.com/python_scripts_beginners_guide/
 ##
 # Define a dictionary
-customers = {
-    '06753':'Mehzabin Afroze',
-    '02457':'Md. Ali', 
-    '02834':'Mosarof Ahmed',
-    '05623':'Mila Hasan',
-    '07895':'Yaqub Ali'
-    }
+machines = {
+    't2.small':     { 'cpu':1, 'ram':2 },
+    't2.medium':    { 'cpu':2, 'ram':4 },
+    't2.large':     { 'cpu':4, 'ram':8 }
+}
 
-# Append a new data
-customers['05634'] = 'Mehboba Ferdous'
+for size in machines:
+    print ("Machine Size",size,"has",machines[size]['cpu'],"CPUs and",machines[size]['ram'],"GB RAM")
 
-print("The customer names are:")
-# Print the values of the dictionary
-for customer in customers:
-    print(customers[customer])
-
-# Take customer ID as input to search
-name = input("Enter customer ID:")
-
-# Search the ID in the dictionary
-found = False
-for customer in customers:
-    if customer == name:
-        found = True
-        print("1. Customer",name,"is",customers[customer])
-        break
-if not Found:
-    print("1. Customer",name,"not found.")
-
-
-# Search the ID in the dictionary
-# test dict.keys()
-if name in customers.keys():
-    print("2. Customer",name,"is",customers[name])
-else:
-    print("2. Customer",name,"not found.")
-
-# test dict.setdefault()
-print("3. Customer",name,"is",customers.setdefault(name))
+size = input("Enter a machine size: ")
 
 # test dict.get()
-print("4. Customer",name,"is",customers.get(name,"not found."))
+if size in machines.keys():
+    print ("Machine Size",size,"has",machines[size]['cpu'],"CPUs and",machines[size]['ram'],"GB RAM")
+else:
+    print ("Invalid machine size.")
