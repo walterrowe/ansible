b9afc6320e16978f5077e2fc637018ab906b3db3
Walter Rowe <walter.rowe@gmail.com>
2021-03-09 06:29:57 -0500

    learn to split strings with re.split

--- test.py
+++ test.py
@@ -1,6 +1,10 @@
+#!/usr/local/bin/python3
+
 ##
 ## https://linuxhint.com/python_scripts_beginners_guide/
 ##
+import sys, re
+
 # Define a dictionary
 machines = {
     't2.small'  : { 'cpu':1, 'ram':2 },
@@ -8,13 +12,18 @@ machines = {
     't2.large'  : { 'cpu':4, 'ram':8 }
 }
 
-for size in machines:
-    print ("Machine Size",size,"has",machines[size]['cpu'],"CPUs and",machines[size]['ram'],"GB RAM")
+if len(sys.argv) > 1:
+    sizes = sys.argv[1:]
+else:
+    size = input("Enter a machine size(s) separated by commas: ")
+    sizes = re.split('\s*[,]\s*',size)
 
-size = input("Enter a machine size: ")
+for size in sizes:
+    print (size)
 
 # test dict.get()
-if size in machines.keys():
-    print ("Machine Size",size,"has",machines[size]['cpu'],"CPUs and",machines[size]['ram'],"GB RAM")
-else:
-    print ("Invalid machine size.")
+for size in sizes:
+    if size in machines.keys():
+        print ("Machine Size",size,"has",machines[size]['cpu'],"CPUs and",machines[size]['ram'],"GB RAM")
+    else:
+        print ("Invalid machine size.")
